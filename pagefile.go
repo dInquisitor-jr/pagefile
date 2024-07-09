f152f80b7c2267a523f480160380c400cb8c764a
iesan <iesan@bloomberg.net>
2024-07-09 11:55:44 -0500

    more work

--- pagefile.go
+++ pagefile.go
@@ -4,7 +4,12 @@ import (
 	"fmt"
 )
 
+// ReadAddress(address int) (byte, error): Reads a byte from the specified address.
+// ReadPage(pageID int) ([]byte, error): Reads an entire page by its ID.
+
 func SearchString(mem *Memory) (string, error) {
-	fmt.Println("works?")
+	// fmt.Println("works?")
+	content, _ := mem.ReadPage(0)
+	fmt.Println(content)
 	return "something", nil
 }
